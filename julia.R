4a59d94ae42599b654bf8c19a7d197c360f5af9a
Gustav Delius <gustav.delius@gmail.com>
2023-05-19 08:00:55 +0100

    Code I used for learning about the code.

--- julia.R
+++ julia.R
@@ -15,8 +15,13 @@ params <- NS_params
 params <- setRateFunction(params, "Rates", "juliaRates")
 params_slow <- setPredKernel(NS_params, getPredKernel(NS_params))
 
+install.packages("microbenchmark")
 library(microbenchmark)
 microbenchmark(getRates(NS_params), getRates(params_slow))
+rates_fns <- lapply(NS_params@rates_funcs, get)
+microbenchmark(mizerRates(NS_params, n = NS_params@initial_n, 
+  n_pp = NS_params@initial_n_pp, effort = NS_params@initial_effort, 
+  rates_fns = rates_fns), times = 1000)
 
 # This does not work yet because get_rates() returns a struct, not a list.
 r <- getRates(params)
@@ -29,4 +34,4 @@ microbenchmark(getRates(NS_params), getRates(params_slow), getRates(params))
 
 microbenchmark(project(NS_params))
 
-microbenchmark(project_simple(NS_params, steps = 1000))
+microbenchmark(project_simple(NS_params, steps = 1000), times = 20)
--- src/test.jl
+++ src/test.jl
@@ -1,5 +1,8 @@
+using Dates, DataFrames, RCall, LoopVectorization, Tullio, LinearAlgebra
 include("params.jl")
 include("rates.jl")
+include("project.jl")
+
 
 R"""
 library(mizer)
@@ -15,17 +18,18 @@ n_pp = copy(params.initial_n_pp);
 effort = copy(params.initial_effort);
 r = get_rates(params, n, n_pp, effort);
 
-import BenchmarkTools: @benc]hmark, @btime
+import BenchmarkTools: @benchmark, @btime
 @btime get_rates(params, n, n_pp, effort);
 # 277.203 μs (35 allocations: 140.28 KiB)
 @btime get_rates!(r, params, n, n_pp, effort);
 # 260.167 μs (20 allocations: 31.14 KiB)
+# 379.200 μs (16 allocations: 432 bytes) on slow notebook
 
 r = get_rates(params, n, n_pp, effort);
 r_mizer = @rget rates_mizer;
 
 isapprox(r.encounter, r_mizer[:encounter])
-isapprox(r.feeding_level, r_mizer[:feeding_level])
+isapprox(r.one_minus_feeding_level, 1.0 .- r_mizer[:feeding_level])
 isapprox(r.e_growth, r_mizer[:e_growth])
 isapprox(r.pred_mort, r_mizer[:pred_mort])
 isapprox(r.mort, r_mizer[:mort])
@@ -33,35 +37,45 @@ isapprox(r.rdd, r_mizer[:rdd])
 isapprox(r.resource_mort, r_mizer[:resource_mort])
 
 # Benchmark individual rate functions
-get_encounter!(r.encounter, r.pred_rate, r.e, params, n, n_pp)
-# Calculate feeding level f_i(w)
-get_feeding_level!(r.feeding_level, params, r.encounter)
-# Calculate the energy available for reproduction and growth
-get_e_repro_and_growth!(r.e, params, r.encounter, r.feeding_level)
-# Calculate the energy for reproduction
-get_e_repro!(r.e_repro, params, r.e)
-# Calculate the growth rate g_i(w)
-get_e_growth!(r.e_growth, r.e_repro, r.e)
+@btime get_encounter!(r.encounter, r.pred_rate, r.e, params, n, n_pp);
+# 89.700 μs (3 allocations: 336 bytes)
 
-## Mortality ----
-# Calculate the predation rate
-get_pred_rate!(r.pred_rate, n, params.pred_rate_kernel, r.feeding_level)
-# Calculate predation mortality on fish \mu_{p,i}(w)
-get_pred_mort!(r.pred_mort, params, n, n_pp, r.pred_rate)
+@btime get_feeding_level!(r.one_minus_feeding_level, params, r.encounter);
+# 2.000 μs (2 allocations: 224 bytes)
+
+@btime get_e_repro_and_growth!(r.e, params, r.encounter, r.one_minus_feeding_level);
+# 4.157 μs (8 allocations: 560 bytes)
+
+@btime get_e_repro!(r.e_repro, params, r.e);
+# 2.011 μs (2 allocations: 224 bytes)
+
+@btime get_e_growth!(r.e_growth, r.e_repro, r.e);
+# 2.067 μs (3 allocations: 336 bytes)
+
+@btime get_pred_rate!(r.pred_rate, n, params.pred_rate_kernel, r.one_minus_feeding_level);
+# 87.700 μs (3 allocations: 432 bytes)
+
+@btime get_pred_mort!(r.pred_mort, params, n, n_pp, r.pred_rate);
 # Calculate fishing mortality
-get_f_mort!(r.f_mort, params, effort)
+@btime get_f_mort!(r.f_mort, params, effort)
 # Calculate total mortality \mu_i(w)
-get_mort!(r.mort, params, r.f_mort, r.pred_mort)
+@btime get_mort!(r.mort, params, r.f_mort, r.pred_mort);
 
 ## Reproduction ----
 # R_di
-get_rdi!(r.rdi, params, n, r.e_repro)
+@btime get_rdi!(r.rdi, params, n, r.e_repro)
 # R_dd,
-get_rdd!(r.rdd, r.rdi, params.species_params.R_max)
+@btime get_rdd!(r.rdd, r.rdi, params.species_params.R_max)
 
 ## Resource ----
 # Calculate mortality on the resource spectrum
-get_resource_mort!(r.resource_mort, params, r.pred_rate)
+@btime get_resource_mort!(r.resource_mort, params, r.pred_rate)
+
+n_pp = copy(params.initial_n_pp)
+temp1 = similar(n_pp)
+temp2 = similar(n_pp)
+
+@btime resource_dynamics!(n_pp, temp1, temp2, params, r, 0.1);
 
 # Test project
 
@@ -88,7 +102,7 @@ using PProf
 
 # Collect an allocation profile
 Profile.Allocs.clear()
-Profile.Allocs.@profile project(params, effort = effort);
+Profile.Allocs.@profile project(params, effort = effort, num_steps = 10000);
 
 # Export pprof allocation profile and open interactive profiling web interface.
 PProf.Allocs.pprof()
