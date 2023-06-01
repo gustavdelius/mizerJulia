d5f377c9e962c94d4681a4692382314e4b257adc
Gustav Delius <gustav.delius@gmail.com>
2023-06-01 22:45:28 +0100

    Adding NS_params

--- src/Mizer.jl
+++ src/Mizer.jl
@@ -1,11 +1,19 @@
 module Mizer
 
+# Export Types
+export Params, Rates, Sim
+# Export functions
 export project, get_rates, get_rates!
+# Export constants
+export NS_params
 
-using Dates, DataFrames, RCall, LoopVectorization, Tullio, LinearAlgebra
+using Dates, DataFrames, RCall, LoopVectorization, Tullio, LinearAlgebra, Serialization
 include("params.jl")
 include("sim.jl")
 include("rates.jl")
 include("project.jl")
+
+datapath = 
+NS_params = deserialize(pkgdir(Mizer, "data", "NS_params.jls"))
 
 end
